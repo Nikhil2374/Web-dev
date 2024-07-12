9b4b903cabcaa1eb7ac88a752a78d527a6f967c3
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-07-12 11:21:23 +0530

    .

---
+++ Javascript/Part 6(JS in Browser)/Practise/script.js
@@ -0,0 +1,7 @@
+/*Let‘s Practice
+Qs. Create a H2 heading element with text - “Hello JavaScript”.
+Append “from Apna Collegestudents” to this text using JS. */
+
+let h2 = document.querySelector("h2")
+console.dir(h2.innerText);
+h2.innerText = h2.innerText + " from Apna College students";
--- Javascript/Part 6(JS in Browser)/script.js
+++ Javascript/Part 6(JS in Browser)/script.js
@@ -17,3 +17,56 @@ document.body.style.background = " brown";
 document.body.childNodes[1].innerText = "eg";
 
 //DOM Manipulation---------------------------------------------------------------------------------------------------
+//Selecting with id===================================================
+let element = document.getElementById('h1');
+console.dir(element);
+//Selecting with class (Gives HTML Collection simmilarily to Array with lenngth etc property)
+let element2 = document.getElementsByClassName('h2');
+console.dir(element2);
+//Selecting with tag
+let element3 = document.getElementsByTagName("p");
+console.dir(element3);
+
+//Query Selector (BEST)===============================================
+//Single Element
+let singleElement = document.querySelector("p");
+console.dir(singleElement);
+//All Elemet
+let allElement = document.querySelectorAll("p");
+console.dir(allElement);
+//For .class
+let classElement = document.querySelector(".h2");
+console.dir(classElement);
+//For #id
+let idElement = document.querySelector("#h1");
+console.dir(idElement);
+
+//DOM Properties-------------------------------------------------------------------------------------------
+//(Get || Set/Change/Update )
+
+//tagName : returns tag for element nodes
+let divelement = document.querySelector("#div");
+console.log(divelement);
+console.log(divelement.tagName);
+
+//innerText : returns the text content of the element and all its children
+console.log(divelement.innerText);
+
+//innerHTML : returns the plain text or HTML contents in the element
+console.log(divelement.innerHTML);
+/*  divelement.innerText = "HLO"; Changes made to div   */
+
+//textContent : returns textual content even for hidden elements
+console.log(divelement.textContent);
+
+//eg: Changing h1 
+//idElement.innerText = "Yellow";
+
+//Navigation:-     Parent Child Property of DOM for Manipulation
+//Children Property 
+
+console.log(divelement.children);
+//First Child
+console.log(divelement.firstChild);
+//Last Child
+console.log(divelement.lastChild);
